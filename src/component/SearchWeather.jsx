9a1b558bc381559ac2b6e44519ab27e6342c402c
Ujjawal Kumar <kujjawal623@gmail.com>
2023-05-23 13:18:37 +0530

    This is the first commit

---
+++ src/component/SearchWeather.jsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from 'react'
+import './styles.css'
+
+export default function SearchWeather() {
+    const [search, setSearch] = useState("Delhi");
+    const [data, setData] = useState([]);
+    const [input, setInput] = useState("");
+    let componentMounted = true;
+    useEffect(() => {
+        const fetchWeather = async () => {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&APPID=eaf4cf87b2c3b62470b545c9105b3535`);
+            if (componentMounted) {
+                setData(await response.json());
+                console.log(data);
+            }
+            return () => {
+                componentMounted = false;
+            }
+        }
+        fetchWeather();
+    }, [search]);
+
+    let emoji = null;
+    if(typeof data.main != "undefined")
+    {
+        if(data.weather[0].main == "Clouds"){
+        emoji = "fa-cloud"
+        }
+        else if(data.weather[0].main=="Thunderstorm"){
+        emoji = "fa-bolt"
+        }
+        else if(data.weather[0].main=="Drizzle"){
+        emoji = "fa-cloud-rain"
+        }
+        else if(data.weather[0].main=="Rain"){
+        emoji = "fa-cloud-shower-heavy"
+        }
+        else if(data.weather[0].main=="Snow"){
+        emoji = "fa-snow-flake"
+        }
+        else{
+        emoji = "fa-smog"
+        }
+    }
+    else  {
+        return (
+            <div>...Loading</div>
+        )
+    }
+
+    let temp = (data.main.temp).toFixed(2);
+    let temp_min = (data.main.temp_min).toFixed(2);
+    let temp_max = (data.main.temp_max).toFixed(2);
+
+    //Date
+    let d = new Date();
+    let date = d.getDate();
+    let year = d.getFullYear();
+    let month = d.toLocaleString("default", {month:'long'});
+    let day = d.toLocaleString("default", {weekday:'long'})
+
+    //Time
+    let time = d.toLocaleString([], {
+        hour : '2-digit',
+        minute: '2-digit',
+        second:'2-digit'
+    });
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        setSearch(input);
+    }
+    return (
+        <div>
+            <h1 className='mytext'>Weather At Your Click!</h1>
+            <div className='container mt-5'>
+                <div className='row justify-content-center'>
+                    <div className='col-md-4'>
+                        <div class="card text-white text-center border-0">
+                            <img src={`https://source.unsplash.com/random/700x900/?${data.weather[0].main}`} class="card-img" alt="..." />
+                            <div class="card-img-overlay">
+                                <form onSubmit={handleSubmit}><div class="input-group mb-4 w-75 mx-auto">
+                                    <input type="search" class="form-control" placeholder="Search City/Zip Code" aria-label="Search City" aria-describedby="basic-addon2" name = "search"
+                                    value={input} onChange={(e)=>setInput(e.target.value
+                                    )}
+                                    required />
+                                    <button type="submit" class="input-group-text" id="basic-addon2">
+                                        <i className='fas fa-search'></i>
+                                    </button>
+                                </div>
+                                </form>
+                                <div className='bg-dark bg-opacity-50 py-3'>
+                                    <h2 class="card-title">{data.name}</h2>
+                                    <p class="card-text lead">{day}, {month} {date}, {year}
+                                    <br />
+                                    {time}
+                                    </p>
+                                    <hr />
+                                    <i className={`fas ${emoji} fa-4x`}></i>
+                                    <h1 className='fw-bolder mb-5 '>{temp} &deg;C</h1>
+                                    <p className="lead fw-bolder mb-0">{data.weather[0].main}</p>
+                                    <p className="lead">{temp_min}&deg;C | {temp_max}&deg;C</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
